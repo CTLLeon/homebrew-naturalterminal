38eb452eb8af630c1ed5f0d1c99379596c315181
Leon Chan <leon.chan@thisisgusto.com>
2025-03-18 17:24:15 +0800

    feat: update sha

--- naturalterminal.rb
+++ naturalterminal.rb
@@ -2,7 +2,7 @@ class Naturalterminal < Formula
     desc "A natural language terminal tool"
     homepage "https://github.com/CTLLeon/homebrew-naturalterminal" # Your main repo URL
     url "https://github.com/CTLLeon/homebrew-naturalterminal/releases/download/v1.0.0/NaturalTerminal-macos-v1.0.0.tar.gz"
-    sha256 "3098ac2ceeae8d8d8a8aed30b53121054a1e75b3b4804353689cb1602522b2db" # Paste the checksum from Step 3
+    sha256 "2b77ebc9e66355a617cc2da02bca807184e65903684daeae6550955d9876ffce" # Paste the checksum from Step 3
     version "1.0.0"
   
     def install
